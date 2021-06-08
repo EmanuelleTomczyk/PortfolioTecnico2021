c644d3d42ee92c9d7af1818e23c15f0bce1c49a7
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-07 21:17:03 -0300

    Update votação.sh

--- FundamentosTI/Exemplos/votação.sh
+++ FundamentosTI/Exemplos/votação.sh
@@ -1,2 +1,22 @@
 # Autora: Emanuelle L. Tomczyk 
 # Descrição: Esse comando é capaz de determinar as pessoas que já possuem idade para votar. 
+
+#bin/bash
+echo "Qual é o ano atual?" 
+read ano_atual
+echo "Qual ano que você nasceu? 
+read ano_nasc
+
+ano_18=$((ano_atual-18))
+ano_16=$((ano_atual-16))
+
+if [ $ano_nasc - le $ano_18 ]
+then 
+      echo "Você deve votar!"
+      
+elif [ $ano_nasc -le $ano_16 ] 
+      echo "Você pode votar!"
+
+else 
+      echo "Você não pode votar!" 
+fi
