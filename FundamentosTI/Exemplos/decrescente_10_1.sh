baf22a12bccc323d8ecbacdaa64575c160cf3135
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-07 21:32:04 -0300

    Update decrescente_10_1.sh

--- FundamentosTI/Exemplos/decrescente_10_1.sh
+++ FundamentosTI/Exemplos/decrescente_10_1.sh
@@ -1,2 +1,10 @@
-# Autora: Emanuelle L. Tomczyk 
-# Descrição: Esse algoritmo imprime os números de 1 a 10 em ordem decrescente. 
+#Autora: Emanuelle L. Tomczyk 
+#Descrição: Esse algoritmo imprime os números de 1 a 10 em ordem decrescente. 
+
+#!/bin/bash
+i=10
+while [ $i -ge 1 ] 
+do 
+      echo " - $i" 
+      i=$((i-1))
+done 
