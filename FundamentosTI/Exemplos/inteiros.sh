57ca4915a32173e32c5a2530546fb3577be72fa0
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-07 21:29:51 -0300

    Update inteiros.sh

--- FundamentosTI/Exemplos/inteiros.sh
+++ FundamentosTI/Exemplos/inteiros.sh
@@ -1,2 +1,15 @@
-# Autora: Emanuelle L. Tomczyk 
-# Descrição: Esse comando é imprime os valores inteiros entre 1 e x. 
+#Autora: Emanuelle L. Tomczyk 
+#Descrição: Esse comando é imprime os valores inteiros entre 1 e x. 
+
+#!/bin/bash 
+echo "Digite um número."
+read x 
+if [ $x -gt 0 ] 
+then 
+      for i in $(seq 1 $x)
+      do 
+            echo "- $i" 
+      done 
+else 
+      echo "Digite um número maior que 0." 
+fi  
