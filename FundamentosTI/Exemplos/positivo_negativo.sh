9e96d9e48bbe179f8f5267da04cc61453c0c2be4
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-07 21:26:18 -0300

    Update positivo_negativo.sh

--- FundamentosTI/Exemplos/positivo_negativo.sh
+++ FundamentosTI/Exemplos/positivo_negativo.sh
@@ -1,2 +1,13 @@
-# Autora: Emanuelle L. Tomczyk 
-# Descrição: Esse comando é capaz de detectar se um número é positivo ou negativo. 
+#Autora: Emanuelle L. Tomczyk 
+#Descrição: Esse comando é capaz de detectar se um número é positivo ou negativo. 
+
+#!/bin/bah 
+echo "Digite um número"
+read x 
+if [ $x -lt 0 ] 
+then 
+      echo "Esse número é negativo"
+      
+else [ $x -ge 0 ] 
+      echo "Esse número é positivo"
+fi 
