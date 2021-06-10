1ac1038b363b53e68e1a9fddd569e68e24f675ec
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-10 09:55:13 -0300

    Update vetor_nome_while.sh

--- FundamentosTI/Vetores/vetor_nome_while.sh
+++ FundamentosTI/Vetores/vetor_nome_while.sh
@@ -1,1 +1,16 @@
+# Autor: Emanuelle L. Tomczyk 
+# Descrição: Vetor utilizando while.
 
+#!/bin/bash 
+vetor[0]="Ísis"
+vetor[1]="Lívia"
+vetor[2]="Mabel"
+
+tam_vetor=${#vetor[@]}
+
+nome=0
+while [ $nome -lt $tam_vetor ] 
+do
+      echo "Oi, eu sou ${vetor[nome]}"
+      nome=$((nome+1))
+done
