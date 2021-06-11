d3d6bc54edf8731c0576d4d3b37b57c81f594096
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2021-06-11 08:43:15 -0300

    Update loop_nome.sh

--- FundamentosTI/Vetores/loop_nome.sh
+++ FundamentosTI/Vetores/loop_nome.sh
@@ -1,1 +1,21 @@
+# Autora: Emanuelle L. Tomczyk 
+# Descrição: Código utilizando loops com código de parada. Solicita nomes que serão armazenados em um vetor.
 
+#!/bin/bash
+cod_parada="exit"
+nome=""
+echo "O código de parada é: exit"
+while [[ $nome != $cod_parada ]] 
+do 
+      echo "Escreva um nome:"
+      read nome 
+      if [[ $nome != $cod_parada ]] 
+      then 
+            vetor[$i]=$nome
+            i=$((i+1))
+            echo "O nome escolhido foi $nome
+__________________________________"
+       fi 
+done 
+echo 
+echo "Valores do vetor: ${#vetor[@]}
