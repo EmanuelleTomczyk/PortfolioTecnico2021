ace72b8f55491362f6b3e3435c4b74421284feb2
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2022-04-28 15:07:11 -0300

    Update Empresas_parceiras.sql

--- Banco_Dados/Zoologico/Empresas_parceiras.sql
+++ Banco_Dados/Zoologico/Empresas_parceiras.sql
@@ -6,11 +6,11 @@ create table empresas_parceiras(
 	funcao varchar
 );
 
-# ADICIONANDO INFORMAÇÕES A TABELA COM IMPORTAÇÕES:
+-- ADICIONANDO INFORMAÇÕES A TABELA COM IMPORTAÇÕES:
 
 \copy empresas_parceiras from ‘C:\Users\emanuelle_tomczyk\Documents\empresas_parceiras.csv’ delimiter ';' csv header;
 
-# INFORMAÇÕES: 
+-- INFORMAÇÕES: 
 cod;nome;email;aluguel;funcao
 64729400;AliZoo;alizoo@gmail.com;2500.00;Alimentacao
 64729422;Brinque;brinque@gmail.com;3000.50;Diversao
