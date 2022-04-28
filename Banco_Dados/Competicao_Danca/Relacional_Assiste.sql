b60f0367febfeed33e424e8c0135ca188e73a230
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2022-04-28 15:20:21 -0300

    Delete Banco_Dados directory

--- Banco_Dados/Competicao_Danca/Relacional_Assiste.sql
+++
@@ -1,6 +0,0 @@
-create table assiste(
-	id_apresentacao int, 
-	id_espectador varchar,
-	foreign key (id_apresentacao) references apresentacao,
-	foreign key (id_espectador) references espectador
-);
--- Banco_Dados/Zoologico/Relacional_Alimentam.sql
+++
@@ -1,6 +0,0 @@
-create table alimentam(
-	cod_estoque int,
-	cod_animal int,
-	foreign key (cod_estoque) references estoque, 
-	foreign key (cod_animal) references animal
-);
