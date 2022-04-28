43dfce6b5df2ce7423817ca7a19e7b206d46984c
EmanuelleTomczyk <83766390+EmanuelleTomczyk@users.noreply.github.com>
2022-04-28 15:07:34 -0300

    Update and rename Contas.txt to Contas.sql

--- Banco_Dados/Zoologico/Contas.sql
+++ Banco_Dados/Zoologico/Contas.sql
@@ -8,11 +8,11 @@ create table contas(
 	func_total decimal(9,2)
 ); 
 
-# ADICIONANDO INFORMAÇÕES A TABELA COM IMPORTAÇÕES:
+-- ADICIONANDO INFORMAÇÕES A TABELA COM IMPORTAÇÕES:
 
 \copy contas from ‘C:\Users\emanuelle_tomczyk\Documents\contas.csv’ delimiter ';' csv header;
 
-# INFORMAÇÕES: 
+-- INFORMAÇÕES: 
 estoque_total;mes_ano;bilheteria_total;gastos_extras;lucro;func_total
 450;30/01/2022;12000.50;1500.75;798.75;9700.50
 300;28/02/2022;15000.75;1000.75;4299.50;9700.50
